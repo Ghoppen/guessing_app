c68b264b01432ea51ddbf99948f207e1f46f5c1d
Ghoppen <Gertautas1@gmail.com>
2022-02-28 13:39:01 +0100

    Revert "Gameove"
    This reverts commit fcdaef2da457b1fce6059a5b9892851999718ac6.

--- screen/GameOver.js
+++ screen/GameOver.js
@@ -1,20 +1,10 @@
 import React from "react";
-import { View, Text, StyleSheet, Button } from "react-native";
-import colors from "../constants/colors";
+import { View, Text, StyleSheet } from "react-native";
 
 const GameOverScreen = (props) => {
   return (
     <View style={styles.screen}>
       <Text>Game is over</Text>
-      <Text>Number of Rounds: {props.numberOfRounds}</Text>
-      <Text>Number was: {props.userNumber}</Text>
-      <View>
-        <Button
-          title="New Game"
-          color={colors.primary}
-          onPress={props.onRestart}
-        />
-      </View>
     </View>
   );
 };
